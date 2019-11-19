44a07ebdbb7c4cb05f65faae57cfa14db3e52fb3
jameslindsaycs <j.lindsay1@nuigalway.ie>
2019-11-19 18:40:59 +0000

    improved checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,11 @@ class Order < ApplicationRecord
         "Purchase Order" => 2
     }
 
+    def add_line_items_from_cart(cart)
+        cart.line_items.each do |item|
+          item.cart_id = nil
+          line_items << item
+        end
+      end
+
 end
